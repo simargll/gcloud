87286fadb7a90756c16b1ac6a0a926f0e0b7a497
simargll <simargll@gmail.com>
2020-11-26 09:26:17 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -5,5 +5,5 @@
 `sudo echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list`
 `sudo apt-get update`
 
-`sudo systemctl enable mongod`
-`sudo systemctl start mongod`
+`sudo systemctl enable mongodb`
+`sudo systemctl start mongodb`
