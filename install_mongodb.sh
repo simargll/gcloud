02aaf8b73f6286f0a24b07b04d32695cd4830a1f
simargll <simargll@gmail.com>
2020-11-26 09:44:06 +0300

    Update install_mongodb.sh

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -2,7 +2,7 @@
 `apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927`
 `apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv D68FA50FEA312927`
 
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list
+`echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list`
 apt-get update
 apt-get install -y mongodb-org
 systemctl enable mongodb
